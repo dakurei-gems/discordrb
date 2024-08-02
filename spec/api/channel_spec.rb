bbf60170efd212596595d771f2c697fc360fd5be
Dakurei <maxime.palanchini@gmail.com>
2024-08-02 12:03:22 +0200

    Fix specs

--- spec/api/channel_spec.rb
+++ spec/api/channel_spec.rb
@@ -20,7 +20,8 @@ describe Discordrb::API::Channel do
           anything,
           channel_id,
           :get,
-          "#{Discordrb::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/test?limit=27&before=#{before_id}&after=#{after_id}",
+          "#{Discordrb::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/test",
+          {limit: 27, before: before_id, after: after_id},
           any_args
         )
       Discordrb::API::Channel.get_reactions(token, channel_id, message_id, "test", before_id, after_id, 27)
@@ -32,7 +33,8 @@ describe Discordrb::API::Channel do
           anything,
           channel_id,
           :get,
-          "#{Discordrb::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/%F0%9F%91%8D?limit=27&before=#{before_id}&after=#{after_id}",
+          "#{Discordrb::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/%F0%9F%91%8D",
+          {limit: 27, before: before_id, after: after_id},
           any_args
         )
       Discordrb::API::Channel.get_reactions(token, channel_id, message_id, "\u{1F44D}", before_id, after_id, 27)
@@ -44,7 +46,8 @@ describe Discordrb::API::Channel do
           anything,
           channel_id,
           :get,
-          "#{Discordrb::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/test?limit=100&before=#{before_id}&after=#{after_id}",
+          "#{Discordrb::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/test",
+          {limit: 100, before: before_id, after: after_id},
           any_args
         )
       Discordrb::API::Channel.get_reactions(token, channel_id, message_id, "test", before_id, after_id, nil)
